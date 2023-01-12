96f73fb80ebf21472c7adc5b94eb6a7125af0b1e
kjolibois <khaliljolibois@gmail.com>
2023-01-12 06:19:42 -0500

    new

---
+++ pages/careerverseworlds/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <main>
+    <section v-if="posts" class="w-full max-w-5xl mx-auto">
+      <h1 class="title">Careerverse worlds</h1>
+      <posts post-type="worlds" :amount="20" />
+    </section>
+  </main>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, error }) {
+    let posts;
+    try {
+      posts = await $content("CvWorlds").fetch();
+    } catch (e) {
+      error({ message: "worlds posts not found" });
+    }
+    return { posts };
+  },
+}
+</script>
